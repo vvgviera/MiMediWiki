96c9a8358e1aef6971986daa14a00c10d6632e0d
vvgviera <vvg.viera1971@gmail.com>
2025-08-28 11:11:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ const articles = [
 	{ title: "Duodenum", url: "page45.html" },
 	{ title: "Jejunum", url: "page46.html" },
 	{ title: "Ileum", url: "page47.html" },
+	{ title: "Cecum", url: "page48.html" },
 	
 	{ title: "Escharotomy", url: "page80.html" },
 	{ title: "Subungual hematoma", url: "page81.html" },
@@ -89,6 +90,7 @@ input.addEventListener("input", () => {
     resultsList.appendChild(li);
   });
 });
+
 
 
 
